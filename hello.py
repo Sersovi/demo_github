9bd4f1337f521816e992dbd1de8a058f57fadf46
Sersovi <sersovi@pm.me>
2019-04-06 13:29:44 +0200

    mas info sobre git

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 # Dentro del directorio root del projecto
 git status (ver el estado de tu projecto git)
-
+git log (Historial de todos los commits)
 git init (Inicializar projecto git)
+git diff (Ver las differencias respecto a el ultimo commit)
 
 git add <FILE>
 
@@ -10,3 +11,6 @@ git commit -m "commentario"
 git push
 
 git set remote http://remote.com
+# Configurar git primera vex instalado
+# git config --global user.email "@.."
+# git config --global user.name "Your Name"
