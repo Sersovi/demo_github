a2d1a9fdbd0b2ee48f5466bcac69490da9d1bac8
Sersovi <sersovi@pm.me>
2019-04-06 13:46:30 +0200

    aun mas commandos git

--- hello.py
+++ hello.py
@@ -5,7 +5,9 @@ git init (Inicializar projecto git)
 git diff (Ver las differencias respecto a el ultimo commit)
 git remote -v (Ver que repositiorio de tu projecyto actual es el remoto)
 
-git add <FILE>
+git add <FILE> # Anadir un archivo concreto para tu commit
+git add . # Anadir todos los artchiuvos cambiados de la carpeta actual para tu commit
+git add  <DIRECTORIO> # Anadir todos los artchiuvos cambiados de la carpeta especificada para tu commit
 
 git commit -m "cualquier commentario"
 
@@ -15,6 +17,9 @@ git push
 git set remote http://remote.com
 # Para linkear un projecto link a un repositorio remoto
 # git remote add origin https://github.com/Sersovi/demo_github.git
+
+# Para descargar un projecto remote
+# git clone https://github.com/Sersovi/demo_github.git
 
 # Configurar git primera vex instalado
 # git config --global user.email "@.."
