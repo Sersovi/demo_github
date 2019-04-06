4cc6e8fdd97ebc2cc29566fcda8a1771e8b3a8fc
Sersovi <sersovi@pm.me>
2019-04-06 13:23:18 +0200

    archivo explicando algunos comandos de git

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Dentro del directorio root del projecto
+git status (ver el estado de tu projecto git)
+
+git init (Inicializar projecto git)
+
+git add <FILE>
+
+git commit -m "commentario"
+
+git push
+
+git set remote http://remote.com
